32311b387dd340825d3dc1e16d91e48262ab93dd
ohwwh <ohw1237@gmail.com>
2022-06-02 00:56:58 +0900

    push_swap_viewer

--- philosopher.c
+++ philosopher.c
@@ -1,12 +1,27 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <pthread.h>
+
+void    *grab_the_fork(void *data)
+{
+    int i;
+
+    printf("%dth philosopher grab the fork!\n", *(int *)data);
+    return ((void *)i);
+}
 
 int main(void)
 {
-    struct timeval stv, etv;
-    gettimeofday(&stv, 0);
-    usleep(1);
-    gettimeofday(&etv, 0);
-    printf("%d\n", etv.tv_usec - stv.tv_usec);
+    pthread_t thread_t[5];
+    int status;
+    int n = 1;
+
+    printf("%dth philosopher grab the fork!\n", n ++);
+    while (n < 7)
+    {
+        pthread_create(&thread_t[n - 2], NULL, grab_the_fork, (void *)&n);
+        pthread_join(thread_t[n - 2], (void **)&status);
+        printf("return thread %d\n", n ++);
+    }
 }
