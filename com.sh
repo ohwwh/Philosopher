fffc2fa5d03ff34d32a34cec8e80faf07a7c8f3f
ohw1237@naver.com <ohw1237@naver.com>
2022-06-03 18:50:06 +0900

    머리  아파

---
+++ com.sh
@@ -0,0 +1,1 @@
+gcc -lpthread philosopher.c -o philosopher
--- philosopher.c
+++ philosopher.c
@@ -2,43 +2,57 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <stdint.h>
 
-int number_of_fork = 6;
+int afork[5] = {2, 2, 2, 2, 2};
 int start;
+pthread_mutex_t mutex_s;
+pthread_mutex_t mutex_n;
 
 void    *grab_the_fork(void *data)
 {
-    int i;
-
+    uintptr_t n = (int)data;
 
     struct timeval mytime;
+    pthread_mutex_lock(&mutex_s);
     gettimeofday(&mytime, NULL);
-    number_of_fork --;
-    printf("at %d %dth philosopher grab the fork! fork is %d\n", mytime.tv_usec - start,  *(int *)data, number_of_fork);
+    afork[(n - 1 + 5) % 5] --;
+    pthread_mutex_unlock(&mutex_s);
+    printf("at %d %luth philosopher grab the left fork! [%d], [%d], [%d], [%d], [%d]\n", mytime.tv_usec - start,  n, afork[0], afork[1], afork[2], afork[3], afork[4]);
+    pthread_mutex_lock(&mutex_s);
+    gettimeofday(&mytime, NULL);
+    afork[(n - 2 + 5) % 5] --;
+    pthread_mutex_unlock(&mutex_s);
+    printf("at %d %luth philosopher grab the right fork! [%d], [%d], [%d], [%d], [%d]\n", mytime.tv_usec - start,  n, afork[0], afork[1], afork[2], afork[3], afork[4]);
 
-  
-    return ((void *)i);
+    return ((void *)n);
 }
 
 int main(void)
 {
     pthread_t thread_t[5];
     int status;
+    int j = 1;
     int n = 1;
-
+    int index[5];
 
     struct timeval mytime;
     gettimeofday(&mytime, NULL);
     start = mytime.tv_usec;
+    pthread_mutex_init(&mutex_s, 0);
+    pthread_mutex_init(&mutex_n, 0);
     gettimeofday(&mytime, NULL);
-    printf("at %d %dth philosopher grab the fork!\n",mytime.tv_usec - start, n ++);
-    while (n < 7)
+    while (n < 6)
     {
-        pthread_create(&thread_t[n - 2], NULL, &grab_the_fork, (void *)&n);
-        pthread_detach(thread_t[n - 2]);
-        //printf("return thread %d\n", n ++);
+        pthread_create(&thread_t[n - 1], NULL, grab_the_fork, (void *)(uintptr_t)n);
         n ++;
     }
+    while (j < 6)
+    {
+        pthread_join(thread_t[j - 1], NULL);
+        j ++;
+    }
+    //while(1);
 }
 //포크를 집는 시간: 31 25 31 25
 //41 32 23 22 31
