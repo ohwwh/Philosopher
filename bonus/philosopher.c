9fe7cf8dc6f50a5e6536973ba62065825a9a6e32
ohw1237@naver.com <ohw1237@naver.com>
2022-06-14 19:21:57 +0900

    철학자 - bonus

--- bonus/philosopher.c
+++ bonus/philosopher.c
@@ -4,21 +4,18 @@ int	main(int argc, char *argv[])
 {
 	int		j;
 	t_philo	*philo;
+	int		pid;
+	int		n;
 
-	if (argc != 6 && argc != 5)
-		return (printf("error!\n"));
+	n = 10;
+	/*if (argc != 6 && argc != 5)
+		return (printf("Arguments error!\n"));*/
 	j = 0;
-	philo = philo_init(argc, argv);
-	while (j < philo->sh_info->philo_num)
+	while (j < n)
 	{
-		pthread_create(&(philo[j].thread_t), 0, routine, (void *)(&philo[j]));
-		pthread_detach(philo[j].thread_t);
-		j ++;
+		if (!fork())
+		{
+			printf("%d process\n", j + 1);
+		}
 	}
-	monitoring(philo, philo->sh_info->philo_num);
-	j = 0;
-	while (j < philo->sh_info->philo_num)
-		pthread_mutex_destroy(&(philo->sh_info->mutex_s[j ++]));
-	pthread_mutex_destroy(&(philo->sh_info->mutex_c));
-	free_all(philo);
 }
--- mandatory/philosopher.c
+++ mandatory/philosopher.c
@@ -6,7 +6,7 @@ int	main(int argc, char *argv[])
 	t_philo	*philo;
 
 	if (argc != 6 && argc != 5)
-		return (printf("error!\n"));
+		return (printf("Arguments error!\n"));
 	j = 0;
 	philo = philo_init(argc, argv);
 	while (j < philo->sh_info->philo_num)
